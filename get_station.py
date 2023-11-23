11e205ba432be286114ae924022605271702e9d8
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-11-23 17:47:54 +0100

    first commit

---
+++ get_station.py
@@ -0,0 +1,46 @@
+import json
+import time
+import urllib.request
+from kafka import KafkaProducer
+
+API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+url = "https://api.jcdecaux.com/vls/v1/stations?apiKey={}".format(API_KEY)
+
+producer = KafkaProducer(bootstrap_servers="localhost:9092")
+while True:
+    response = urllib.request.urlopen(url)
+    stations_data = json.loads(response.read().decode())
+
+    for station in stations_data:
+        # Check if 'position' key exists
+        if 'position' in station:
+            # Check if 'lat' and 'lng' keys exist under 'position'
+            latitude = station["position"]["lng"] if "position" in station and "lat" in station["position"] else None
+            longitude = station["position"]["lat"] if "position" in station and "lng" in station["position"] else None
+            # Extract relevant fields to match the consumer schema
+            formatted_station = {
+                "number": station["number"] if "number" in station else None,
+                "contractName": station["contract_name"] if "contract_name" in station else "",
+                "name": station["name"] if "name" in station else "",
+                "address": station["address"] if "address" in station else "",
+                "last_update":station["last_update"],
+                "position": {
+                    "latitude": latitude,
+                    "longitude": longitude
+                },
+                "totalStands": {
+                    "availabilities": {
+                        "bikes": station["available_bikes"] if "available_bikes" in station else 0,
+                        "stands": station["bike_stands"] if "bike_stands" in station else 0
+                    },
+                    "capacity": station["bike_stands"] if "bike_stands" in station else 0
+                }
+            }
+
+
+        # Send formatted data to Kafka topic
+        producer.send("velib-stations", json.dumps(formatted_station).encode())
+
+    print("{} Produced {} station records".format(time.time(), len(stations_data)))
+    time.sleep(1)
